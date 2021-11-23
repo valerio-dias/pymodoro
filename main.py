b61e5cda41d6be24b3b58b36b05b2c48bf5192d8
Valerio Dias <valeriods@gmail.com>
2021-11-23 19:10:29 -0300

    Criado alarme e insercao de tempo

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from turtle import Turtle, Screen
 import datetime
 from relogio import Relogio
+import pygame
 
+pygame.mixer.init()
+ring = pygame.mixer.Sound('god_sound.wav')
 t = Turtle()
 t.hideturtle()
-
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -18,11 +20,14 @@ t.goto(0, 250)
 t.color('white')
 t.clear()
 t.write('PYmodoro', align='center', font=('Comic Sans MS', 30, 'normal'))
+t.goto(0, -250)
+t.color('white')
+t.write('Bom estudo.', align='center', font=('Comic Sans MS', 15, 'normal'))
 
-relogio = Relogio()
-relogio
+iniciar = screen.textinput(title="Início", prompt="Escolha quantos minutos (25/30/45/57:")
 
-
-
+int_ini = int(iniciar) * 60
+relogio = Relogio(int_ini)
+ring.play()
 
 screen.exitonclick()
--- relogio.py
+++ relogio.py
@@ -4,26 +4,24 @@ from turtle import Turtle
 
 
 class Relogio(Turtle):
-    def __init__(self):
+    def __init__(self, segundos):
         super().__init__()
+        self.segundos = segundos
+        self.tempo = 0
         self.color('white')
         self.penup()
         self.hideturtle()
-        self.time_screen = 25
         self.timer()
         self.update_tela()
-   
+
     def timer(self):
-        self.segundos = 1500
         self.tempo = timedelta(seconds=self.segundos)
-        while self.tempo !=  '0:00:00':
+        while str(self.tempo) != '0:00:00':
             sleep(1)
             self.update_tela()
             self.tempo = self.tempo - timedelta(seconds=1)
-            
 
     def update_tela(self):
         self.clear()
         self.goto(0, 0)
         self.write(self.tempo, align="center", font=("Courier", 80, "normal"))
-     
