95932119475b265726090e862033e9c408186538
Valerio Dias <valeriods@gmail.com>
2021-11-24 19:55:04 -0300

    Fixado os tempos do menu

--- main.py
+++ main.py
@@ -31,20 +31,24 @@ continua = 's'
 relogio = Relogio()
 while continua == 's':
     iniciar = screen.textinput(title="Tempo", prompt="Escolha quantos minutos (25/30/45/50:")
+    if iniciar != 25 or 30 or 45 or 50:
+        iniciar = screen.textinput(title="Tempo", prompt="Tempo errado! Escolha quantos minutos (25/30/45/50:")
     int_ini = int(iniciar) * 60
     relogio.timer(int_ini)
     ring.play()
     relogio.update_tela()
 
-    intervalo = screen.textinput(title="Intervalo",prompt="Escolha quantos minutos de intervalo (5/15/30) ou tecle n para encerrar:")
+    intervalo = screen.textinput(title="Intervalo", prompt="Escolha quantos minutos de intervalo (5/15/30) ou tecle n para encerrar:")
+    if intervalo != 5 or 15 or 30:
+        intervalo = screen.textinput(title="Intervalo",  prompt="Intrvalo errado! Escolha quantos minutos de intervalo (5/15/30) ou tecle n para encerrar:")
     int_int = int(intervalo) * 60
-
     relogio.update_tela()
+    # int_ini = int_ini * 5
     relogio.timer(int_int)
     relogio.update_tela()
     elephant.play()
 
-    continua = screen.textinput(title='Inicio/Continua', prompt='Começar ou continuar estudos(s/n)?')
+    continua = screen.textinput(title='Inicio/Continua', prompt='Continuar estudos(s/n)?')
 
     relogio.update_tela()
 
