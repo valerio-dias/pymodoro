6c617c55d3c40ee112fb63b07010c31dbeb53f54
Valerio Dias <valeriods@gmail.com>
2021-11-24 17:01:39 -0300

    Criado continue e enxugado codigo

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from turtle import Turtle, Screen
 from relogio import Relogio
 import pygame
 
-
 pygame.mixer.init()
 ring = pygame.mixer.Sound('god_sound.wav')
 elephant = pygame.mixer.Sound('elephant_sound.wav')
@@ -10,11 +9,13 @@ elephant = pygame.mixer.Sound('elephant_sound.wav')
 t = Turtle()
 t.hideturtle()
 
+
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor(188.0/255, 71.0/255, 73.0/255)
 screen.title('PYmodoro')
 screen.tracer(0)
+
 
 t.clear()
 t.goto(0, 250)
@@ -25,19 +26,26 @@ t.goto(0, -250)
 t.color('white')
 t.write('Bom estudo.', align='center', font=('Comic Sans MS', 15, 'normal'))
 
-iniciar = screen.textinput(title="Início", prompt="Escolha quantos minutos (25/30/45/50:")
+continua = 's'
 
-int_ini = int(iniciar) * 60
-relogio = Relogio(int_ini)
-ring.play()
+relogio = Relogio()
+while continua == 's':
+    iniciar = screen.textinput(title="Tempo", prompt="Escolha quantos minutos (25/30/45/50:")
+    int_ini = int(iniciar) * 60
+    relogio.timer(int_ini)
+    ring.play()
+    relogio.update_tela()
 
-intervalo = screen.textinput(title="Intervalo", prompt="Escolha quantos minutos de intervalo (5/15/30) ou tecle n para encerrar:")
-int_int = int(intervalo) * 60
+    intervalo = screen.textinput(title="Intervalo",prompt="Escolha quantos minutos de intervalo (5/15/30) ou tecle n para encerrar:")
+    int_int = int(intervalo) * 60
 
-relogio.update_tela()
+    relogio.update_tela()
+    relogio.timer(int_int)
+    relogio.update_tela()
+    elephant.play()
 
-relogio.break_time(int_int)
-relogio.update_tela()
-elephant.play()
+    continua = screen.textinput(title='Inicio/Continua', prompt='Começar ou continuar estudos(s/n)?')
+
+    relogio.update_tela()
 
 screen.exitonclick()
--- relogio.py
+++ relogio.py
@@ -3,24 +3,17 @@ from time import sleep
 from turtle import Turtle
 
 class Relogio(Turtle):
-    def __init__(self, segundos):
+    def __init__(self, segundos=0):
         super().__init__()
         self.segundos = segundos
         self.tempo = 0
         self.color('white')
         self.penup()
         self.hideturtle()
-        self.timer()
+        self.timer(segundos)
         self.update_tela()
 
-    def timer(self):
-        self.tempo = timedelta(seconds=self.segundos)
-        while str(self.tempo) != '0:00:00':
-            self.update_tela()
-            self.tempo = self.tempo - timedelta(seconds=1)
-            sleep(1)
-
-    def break_time(self, segundos):
+    def timer(self, segundos):
         self.tempo = timedelta(seconds=segundos)
         while str(self.tempo) != '0:00:00':
             self.update_tela()
